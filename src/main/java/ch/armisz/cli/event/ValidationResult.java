5c1e90b4a9a8f04c487d3d37f7b85f4dee915707
armisz <jakonile8@gmail.com>
2019-11-04 20:24:55 +0100

    Polishing

--- src/main/java/ch/armisz/cli/event/ValidationResult.java
+++ src/main/java/ch/armisz/cli/event/ValidationResult.java
@@ -3,8 +3,6 @@ package ch.armisz.cli.event;
 import ch.armisz.cli.event.internal.EventResult;
 
 public class ValidationResult extends EventResult {
-	
-	  public static final ValidationResult OK = new ValidationResult(Level.INFO, "OK");
 
     public static final ValidationResult OK = new ValidationResult(Level.INFO, "OK");
 
